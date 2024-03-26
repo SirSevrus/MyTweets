f4f717c3983800a92f6f2348683ef4bf8a194530
SirSevrus <155065276+SirSevrus@users.noreply.github.com>
2024-03-26 17:15:12 +0530

    Create addingDigits.py

---
+++ addingDigits.py
@@ -0,0 +1,19 @@
+import sys
+
+two_digit_number = input()  # Taking the number as input in string format.
+
+def findSum(number):
+    '''
+  You can find the sum of the digits by running a for loop taking each element one by one and summing them up and returning it.
+    '''
+  sum = 0
+  for num in number:
+      if not num.isdigit():
+          print('The number you entered is not a number it contains characters other then 0-9 also.')
+          sys.exit()
+  num = int(num)
+  sum += num
+  return sum
+
+sum = findSum(two_digit_number)
+print(sum)
